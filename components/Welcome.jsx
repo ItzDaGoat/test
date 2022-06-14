3fa0a873c94692641d4fb0aaef152c96d3f0239a
JesseB <xmzedu@gmail.com>
2022-06-15 00:23:42 +0800

    0.7 welcome 占一屏

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -2,20 +2,15 @@ import { Link } from "react-scroll"
 
 export const Welcome = () => {
     return (
-        <div className=" relative h-5/6  pt-16">
-            <main className=" relative  h-[40rem]  mx-auto max-w-7xl   px-4   ">
-                <video
-                    autoPlay
-                    loop
-                    muted
-                    className="absolute top-0 right-10 h-[35rem] w-[100rem]"
-                >
+        <div className=" relative  h-screen  pt-16">
+            <main className=" relative  h-[40rem]  mx-auto max-w-7xl   px-4  overflow-hidden  ">
+                <video autoPlay loop muted className="absolute top-5   h-[35rem] w-[100rem]">
                     <source src="/webto3.mp4" type="video/mp4" />
                 </video>
 
                 <div className="relative py-5   sm:text-center lg:text-left  w-8/12    flex  flex-col  justify-start  ">
                     <div className="mb-8  ">
-                        <div className=" mb-4 w-10/12 font-semibold  leading-relaxed text-5xl text-white  ">
+                        <div className="mt-10 mb-4 w-10/12 font-semibold  leading-relaxed text-5xl text-white  ">
                             每一个WEB2公民进入WEB3的新手村
                         </div>
                     </div>
